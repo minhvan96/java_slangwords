88e0cedd1f5d36eac1e318d4d7c6e35de3414a4c
jackson <jackson@appsure.com.au>
2022-07-07 00:25:07 +0700

    feat: update menu

--- src/main/SlangWords.java
+++ src/main/SlangWords.java
@@ -1,10 +1,13 @@
 package main;
 
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class SlangWords {
 
     public SlangWords() {
-
     }
 
     private void ReadDictionary() {
@@ -13,7 +16,40 @@ public class SlangWords {
 
     public static void main(String[] args) {
         System.out.println("Hello");
+
+        // region menu
+        Map<Integer, String> menu = new HashMap<Integer, String>();
+        menu.put(1, "Search by word");
+        menu.put(2, "Search by meaning");
+        menu.put(3, "Search history");
+        menu.put(4, "Add slang");
+        menu.put(5, "Edit slang");
+        menu.put(6, "Delete slang");
+        menu.put(7, "Reset");
+        menu.put(8, "Random slang");
+        menu.put(9, "Slang puzzle");
+        menu.put(10, "Slang puzzle 2");
+        // endregion
+
         SwDictionary dic = new SwDictionary();
+
+        System.out.println("SLANG WORDS APP MENU");
+        menu.forEach((k, v) -> System.out.println(k + ": " + v));
+        System.out.println();
+
+
+        System.out.println("Select option");
+        Scanner optionScanner = new Scanner(System.in);
+        while (optionScanner.hasNextLine()){
+            int option = Integer.parseInt(optionScanner.nextLine());
+            switch (option){
+                case 1:
+                    Scanner searchKeyWordScanner = new Scanner(System.in);
+                    String searchKeyWord = searchKeyWordScanner.nextLine();
+
+            }
+        }
+
         dic.read();
     }
 }
--- src/main/SwDictionary.java
+++ src/main/SwDictionary.java
@@ -4,25 +4,32 @@ import main.Entity.SlangWord;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.util.*;
 
 public class SwDictionary {
+    private ArrayList<SlangWord> dic;
     public SwDictionary() {
-
+        dic = (ArrayList<SlangWord>) read();
     }
 
-    public void read() {
+    public Collection<SlangWord> read() {
         String slangWordsDic = SlangWord.class.getResource("slang.txt").getPath();
         File slangDicFile = new File(slangWordsDic);
+        ArrayList<SlangWord> slangDic = new ArrayList<SlangWord>();
         try {
             Scanner studentReader = new Scanner(slangDicFile);
-
             while (studentReader.hasNextLine()) {
                 String line = studentReader.nextLine();
-                System.out.println(line);
+                String[] splitedLine = line.split("`");
+                String word = splitedLine[0];
+                String meaning = splitedLine[1];
+                SlangWord slangWord = new SlangWord(word, meaning);
+                slangDic.add(slangWord);
             }
+            return slangDic;
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
     }
+
 }
